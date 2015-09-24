509fb8bf3dd10f1b423287a6af763d0fff5462be
Teddy Knox <eknox@linkedin.com>
2015-09-24 16:14:43 -0700

    Add bootstrap

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ var OAuth2 = require('oauth').OAuth2;
 
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
-var csrfState = process.env.CSRF_STATE;
 var domain = process.env.DOMAIN;
 var port = process.env.PORT;
 
@@ -15,44 +14,43 @@ var oauth2 = new OAuth2(
     '/oauth2/authorize',
     '/oauth2/token',
     null);
-
 var redirectUri = 'http://' + domain + ((port == 80) ? '' : (':' + port)) + '/auth/code';
-var authUrl = oauth2.getAuthorizeUrl({
-    redirect_uri: redirectUri,
-    scope: ['shared_playstate'],
-    state: csrfState,
-    response_type: 'code',
-    // email: "eknox@linkedin.com",
-    // showSignup: true
-});
 
-router.get('/', function(req, res, next) {
-  res.redirect(authUrl)
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  if (!req.body.personalEmail || ! req.body.workEmail) {
+    // TODO: Redirect to validation error page
+    return res.redirect('.');
+  } else {
+    var authUrl = oauth2.getAuthorizeUrl({
+        redirect_uri: redirectUri,
+        scope: ['shared_playstate'],
+        response_type: 'code',
+        email: req.body.personalEmail,
+        showSignup: true
+    });
+    // TODO: Fire off an email confirmation task.
+    res.redirect(authUrl)
+  }
 });
 
 router.get('/code', function(req, res, next) {
-  if (!req.query.code || !req.query.state || req.query.state !== csrfState) {
-    console.log("auth code error");
-    console.log(req.query);
-    // throw;
+  if (req.query.error) {
+    next(new Error("Rdio returned status " + req.query.error + " and message " + req.query.errorMessage));
   }
-  var code = req.query.code
   oauth2.getOAuthAccessToken(
     req.query.code,
     { 'grant_type': 'authorization_code',
       'redirect_uri': redirectUri },
-    function (e, access_token, refresh_token, results){
-        if (e) {
-            console.log(e);
-            res.end(e);
+    function (err, access_token, refresh_token, results) {
+        if (err) {
+          next(err);
         } else if (results.error) {
-            // TODO: Conditionally refresh the user's access token.
-            console.log(results);
-            res.end(JSON.stringify(results));
-        }
-        else {
-            console.log('Obtained access_token: ', access_token);
-            res.end(access_token);
+          console.error(results.error);
+          console.log("Results error");
+          res.end(JSON.stringify(results));
+        } else {
+          // TODO: Save access token and refresh_token to user model.
         }
   });
 });
